122236bffd06f07aa4db1b8a6cfe9285b88c3398
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-05 13:45:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_third_tier
     railroads: {
       pieces: 4,
       rent_in_dollars: {
-        one_piece: {},
+        one_piece: {25},
         two_piece: {},
         three_piece: {},
         four_piece: {}
